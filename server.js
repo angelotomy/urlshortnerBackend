6bec48e9237408d767e0c7e9d8601304727e41f8
angelotomy <angelo37tomy@gmail.com>
2025-02-12 18:26:05 +0400

    server changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({ origin: 'https://urlshortener-frontend-azure.vercel.app/' }));
 
 // URL model
-const Url = require('./models/urlModel');
+const Url = require('./models/Url');
 
 // Route for URL shortening
 app.post('/shorten', async (req, res) => {
